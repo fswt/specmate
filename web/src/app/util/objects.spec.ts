92251e3f5c935026d2a9f944b06e96223aa3141a
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2018-10-02 20:38:17 +0200

    remove meta-test

--- web/src/app/util/objects.spec.ts
+++ web/src/app/util/objects.spec.ts
@@ -5,9 +5,6 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('Objects Specmate', () => {
-    it('should work', () => {
-        expect(true).to.be.true;
-    });
     it('should return undefined if both objects are undefined', () => {
         const o1: IContainer = { ___nsuri:'', url: '', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
         const o2: IContainer = undefined;
