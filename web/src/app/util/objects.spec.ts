1c6a6eebe2e326982fdc8a0fb97b4c3295cee008
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2018-10-09 10:37:33 +0200

    fix test, so that it makes sense

--- web/src/app/util/objects.spec.ts
+++ web/src/app/util/objects.spec.ts
@@ -6,20 +6,20 @@ import 'mocha';
 
 describe('Objects Specmate', () => {
     it('should return undefined if both objects are undefined', () => {
-        const o1: IContainer = { ___nsuri:'', url: '', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
+        const o1: IContainer = { ___nsuri: 'string', url: '', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
         const o2: IContainer = undefined;
         expect(Objects.changedFields(o1, o2)).to.be.undefined;
     });
     it('should throw an new error if the objects type is differnt', () => {
-        const o1: IContainer = { ___nsuri:'', url: '12', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
+        const o1: IContainer = { ___nsuri: 'string', url: '12', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
         const o2 = { name: 'd' };
         expect(function () {Objects.changedFields(o1, o2); })
         .to.throw('Types do not match! Tried to get changed fields from unmatching types.');
     });
     it('should return array if the objects has the changed fields', () => {
-        const o1: IContainer = { ___nsuri:'', url: '12', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
-        const o2: IContainer = { ___nsuri:'', url: '12', className: 'string', id : '2', name : 'weeds', description : 'this is weeds' };
-        expect(Objects.changedFields(o1, o2)).to.eql([ 'id', '___nsuri', '___nsuri' ]);
+        const o1: IContainer = { ___nsuri: 'string', url: '12', className: 'string', id : '1', name : 'weeds', description : 'this is weeds' };
+        const o2: IContainer = { ___nsuri: 'string', url: '12', className: 'string', id : '2', name : 'weeds', description : 'this is weeds' };
+        expect(Objects.changedFields(o1, o2)).to.eql([ 'id' ]);
     });
     it('should return changeField if the objects has the different length in array', () => {
         const o1 = { url: '12', className: 'string', id : '1', name : 'weeds', description : 'this is weeds', lines: ['x', 'y', 'z'] };
